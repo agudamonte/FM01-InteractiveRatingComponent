0ab15710459cd2325a5135a71eedcfcee4a9c150
agudamonte <agu.damonte@gmail.com>
2022-12-07 16:18:22 -0300

    adding the dinamics with js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,43 @@
-const rateSelected = document.querySelector(".rates-level-container");
-const submit = document.querySelector(".main-view");
-const rateView = document.querySelector(".main-view");
+const ratingState = document.querySelector("#first-section");
+const thankYouState = document.querySelector("#second-section");
+const rates = document.querySelector("#rates");
+const rateSelected = document.querySelector("#rate-selected");
+const submitBtn = document.querySelector("#btn-submit");
 
-rateSelected.addEventListener("click", seleccionarPuntaje);
-submit.addEventListener("click", submitRate);
+rates.addEventListener("click", select);
+submitBtn.addEventListener("click", submit);
 
-let puntaje = "";
+let rate = "";
 
-function seleccionarPuntaje(e) {
-  let selected = document.querySelector(".selected");
-  if (selected) {
-    selected.classList.remove("selected");
+function select(event) {
+  if (event.target.classList.contains("btn-rate")) {
+    rate = event.target.textContent;
+    cleanAlert();
   }
-  if (e.target.classList.contains("rate")) {
-    e.target.classList.toggle("selected");
-    puntaje = e.target.textContent;
-  }
-  console.log(puntaje);
 }
 
-function submitRate(e) {
-  e.preventDefault();
-  if (e.target.classList.contains("submit-button")) {
-    // rateView.classList.toggle("inactivo");
-    rateView.innerHTML = `
-    <section class="main main-container main-view">
-    <figure class="submit-img"></figure>
-    <div class="rate-submitted"> You selected ${puntaje} out of 5!</div>
-    <h1>Thank You!</h1>
-    <p class="main-p">
-      We appreciate you taking the time to give a rating. If you ever need
-      more support, don't hesitate to get in touch!
-    </p>
-  </section>
-    `;
+function submit(e) {
+  if (rate !== "") {
+    ratingState.classList.toggle("inactive");
+    thankYouState.classList.toggle("inactive");
+    //
+    rateSelected.textContent = rate;
+    return;
+  }
+  errorMessage(e.target.parentElement);
+}
+
+function errorMessage(reference) {
+  cleanAlert();
+  const alertMessage = document.createElement("p");
+  alertMessage.classList.add("error");
+  alertMessage.textContent = "Please rate us before submission";
+  reference.append(alertMessage);
+}
+
+function cleanAlert() {
+  alert = document.querySelector(".error");
+  if (alert) {
+    alert.remove();
   }
 }
